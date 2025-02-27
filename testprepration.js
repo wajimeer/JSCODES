0695b9b21ef09c27c3320ec1d14faa2a6d3a2b29
wajimeer <waji21569870@gmail.com>
2025-02-27 14:19:54 +0500

    new code

--- testprepration.js
+++ testprepration.js
@@ -133,3 +133,40 @@ function sumandproduct(arr){
 
 }
 console.log(sumandproduct([1,2,3,4,5]));
+function remv(arr){
+    let hashmap = {}
+    let result = [];
+    for(let i=0; i<arr.length; i++){
+        if(!hashmap[arr[i]]){
+            hashmap[arr[i]]=true;
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(remv([1,2,2,2,3,4,5,6,6,6,7,7,7,8,8,1,10,10,10]))
+function removelast(arr){
+    let n =arr.length-1
+    for(let i=0; i<n;i++){
+        console.log(arr[i])
+    }
+    return arr[n]
+}
+removelast([1,2,3,4,5])
+
+function binarrysearch(arr,target){
+    let left =0;
+    let right = arr.length-1;
+    while(left<=right){
+        let mid = Math.floor(left+right/2)
+        if(arr[mid]===target){
+            return mid;
+        }else if(arr[mid]<target){
+            left = mid +1;
+        }else{
+            right = mid -1
+        }
+    }
+    return -1
+}
+console.log(binarrysearch([1,2,33,4,5,77,8,9],9))
